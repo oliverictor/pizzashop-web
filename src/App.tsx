d196825e5b554f3c480a0b3f542801a9fac85825
victor.oliveira <victor.oliveira@hexagon.pro>
2024-12-17 08:35:45 -0300

    add themeToggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { routes } from './routes'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
 
   return (
     <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Helmet titleTemplate="%s | pizza.shop" />
       <Toaster richColors />
       <RouterProvider router={routes} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
